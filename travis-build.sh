684f4c71dec218453b4d6c471aa78ffaf3dbaea9
UriHerrera <hello@uriherrera.com>
2019-08-17 21:30:34 -0500

    don't add radeon x11 driver

--- travis-build.sh
+++ travis-build.sh
@@ -14,7 +14,7 @@ Section: misc
 Priority: optional
 Homepage: https://nxos.org
 Package: nitrux-hardware-drivers
-Version: 1.0.8.1-${GIT_COMMIT}
+Version: 1.0.8.2-${GIT_COMMIT}
 Maintainer: Uri Herrera <uri_herrera@nxos.org>
 Depends: $DEPENDS
 Architecture: amd64
