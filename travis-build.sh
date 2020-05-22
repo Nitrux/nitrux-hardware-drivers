6e08a041f0a94ef4b27d216355e0732f0a62480a
luis-lavaire <llavaire01@gmail.com>
2020-05-22 13:34:34 -0600

    removed useless code.

--- travis-build.sh
+++ travis-build.sh
@@ -5,20 +5,18 @@ apt-get --yes install wget equivs curl git
 
 cd package/
 
-DEPENDS=$( echo $(sed -e '/^#.*$/d; /^$/d; /^[[:space:]].*$/d' dependencies) | tr ' ' ',')
+deps=$(sed -r '/^(#.*|\s)*$/d; $! s/$/,/' package/dependencies | tr -d \\n)
+git_commit=$(git rev-parse --short HEAD)
 
-GIT_COMMIT=$(git rev-parse --short HEAD)
-
-echo "
-Section: misc
-Priority: optional
-Homepage: https://nxos.org
-Package: nitrux-hardware-drivers
-Version: 1.0.8.17-${GIT_COMMIT}
-Maintainer: Uri Herrera <uri_herrera@nxos.org>
-Depends: $DEPENDS
-Architecture: amd64
-Description: Hardware drivers metapackage for Nitrux.
-" > configuration
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: nitrux-hardware-drivers" \
+	"Version: 1.0.8.17-$git_commit" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Architecture: amd64" \
+	"Description: Hardware drivers metapackage for Nitrux."
 
 equivs-build configuration
