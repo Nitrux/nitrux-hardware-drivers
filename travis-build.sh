6d0b6461cc3894668b3abaa33683b02078ea6087
Uri Herrera <hello@uriherrera.com>
2020-08-25 04:25:21 -0500

    add conflicts

--- travis-build.sh
+++ travis-build.sh
@@ -11,9 +11,10 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-hardware-drivers" \
-	"Version: 1.0.8.18-$git_commit" \
+	"Version: 1.0.9-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
+	"Conflicts: nitrux-hardware-drivers-legacy" \
 	"Architecture: amd64" \
 	"Description: Hardware drivers metapackage for Nitrux."
 
