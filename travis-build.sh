677c21183db88baf195c3851a0e548c17713da97
Uri Herrera <hello@uriherrera.com>
2021-01-29 16:40:33 -0600

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -3,22 +3,19 @@
 apt-get --yes update
 apt-get --yes install wget equivs curl git
 
-cd package/
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
+git_commit=$(git rev-parse --short HEAD)
 
-DEPENDS=$( echo $(sed -e '/^#.*$/d; /^$/d; /^[[:space:]].*$/d' dependencies) | tr ' ' ',')
-
-GIT_COMMIT=$(git rev-parse --short HEAD)
-
-echo "
-Section: misc
-Priority: optional
-Homepage: https://nxos.org
-Package: nitrux-hardware-drivers-minimal
-Version: 1.0.8.18-${GIT_COMMIT}
-Maintainer: Uri Herrera <uri_herrera@nxos.org>
-Depends: $DEPENDS
-Architecture: amd64
-Description: Hardware drivers metapackage for Nitrux.
-" > configuration
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: nitrux-hardware-drivers" \
+	"Version: 1.9.0-$git_commit" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Conflicts: nitrux-hardware-drivers-legacy, nitrux-hardware-drivers" \
+	"Architecture: amd64" \
+	"Description: Hardware drivers metapackage for Nitrux."
 
 equivs-build configuration
