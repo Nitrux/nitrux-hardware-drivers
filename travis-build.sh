5431b9fcc97837ecc67f2963c4f4051954f78767
Uri Herrera <hello@uriherrera.com>
2021-02-26 17:49:35 -0600

    create pkg

--- travis-build.sh
+++ travis-build.sh
@@ -10,8 +10,8 @@ git_commit=$(git rev-parse --short HEAD)
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
-	"Package: nitrux-hardware-drivers-minimal" \
-	"Version: 1.0.9-$git_commit" \
+	"Package: nitrux-hardware-drivers-minimal-legacy" \
+	"Version: 0.0.1-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: nitrux-hardware-drivers-legacy, nitrux-hardware-drivers" \
