658bbe57de57c9e8c6ea54bb1c1ae80ec47cf247
Uri Herrera <hello@uriherrera.com>
2022-12-19 13:04:18 -0600

    update pkg

--- gh-build.sh
+++ gh-build.sh
@@ -11,10 +11,11 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-hardware-drivers-legacy" \
-	"Version: 1.2.8-$git_commit" \
+	"Version: 1.2.9-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: nitrux-hardware-drivers" \
+	"Recommends: broadcom-sta-common, broadcom-sta-dkms, broadcom-sta-source"
 	"Architecture: amd64" \
 	"Description: Hardware drivers metapackage for Nitrux."
 
