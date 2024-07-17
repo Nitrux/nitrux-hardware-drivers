6c53f2b105ef9d59ed7a0f4c98f823c9d24d0cf7
Uri Herrera <hello@uriherrera.com>
2024-07-17 16:46:19 -0600

    add dep

--- gh-build.sh
+++ gh-build.sh
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-hardware-drivers-legacy" \
-	"Version: 1.4.9-$git_commit" \
+	"Version: 1.5.0-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: nitrux-hardware-drivers" \
