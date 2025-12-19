3326ee6956a50b732af7049b220af21ad29a0ceb
Uri Herrera <hello@uriherrera.com>
2025-12-19 09:16:46 -0600

    add dep

--- gh-build.sh
+++ gh-build.sh
@@ -16,7 +16,7 @@ git_commit=$(git rev-parse --short HEAD 2>/dev/null)
 	"Priority: required" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-hardware-drivers" \
-	"Version: 1.7.5-$git_commit" \
+	"Version: 1.7.6-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Recommends: broadcom-sta-common, broadcom-sta-dkms, broadcom-sta-source" \
